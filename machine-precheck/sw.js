10e8cbcbbdc591f8853fdcffaa0c80994083a8a6
utm-3426 <founder@arkaicolabs.com>
2026-09-04 17:42:09 +0100

    Rotate prototype cache to v9

--- machine-precheck/sw.js
+++ machine-precheck/sw.js
@@ -1,5 +1,5 @@
-const CACHE='machine-precheck-prototype-v8';
-const SHELL=['./index.html','./app.css?v=8','./app.js?v=8','./manifest.webmanifest','./v8-sprite-60f.webp?v=8'];
+const CACHE='machine-precheck-prototype-v9';
+const SHELL=['./index.html','./app.css?v=9','./app.js?v=9','./manifest.webmanifest','./v8-sprite-60f.webp?v=9'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).catch(()=>{}));});
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',event=>{if(event.request.mode==='navigate'){event.respondWith(fetch(event.request).catch(()=>caches.match('./index.html')));return;}event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request)));});
