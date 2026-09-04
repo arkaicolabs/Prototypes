8b824a551009691681c7e80021323a7f5f14c184
utm-3426 <founder@arkaicolabs.com>
2026-09-04 17:24:42 +0100

    Refresh prototype cache for frame player

--- machine-precheck/sw.js
+++ machine-precheck/sw.js
@@ -1,5 +1,11 @@
-const CACHE='machine-precheck-prototype-v2';
-const SHELL=['./','./index.html','./app.css','./app.js','./manifest.webmanifest','./machine-pre-check-list-intro-login-v8.mp4'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).catch(()=>{})));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request))));
+const CACHE='machine-precheck-prototype-v3';
+const SHELL=['./index.html','./app.css?v=3','./app.js?v=3','./manifest.webmanifest','./v8-sprite-60f.webp?v=3'];
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).catch(()=>{}));});
+self.addEventListener('activate',event=>event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',event=>{
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request)));
+});
