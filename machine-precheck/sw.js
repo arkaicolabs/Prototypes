af05d7d0a4c81677f517d8402d8015ee3d246d69
utm-3426 <founder@arkaicolabs.com>
2026-09-04 20:19:20 +0100

    Rotate cache for title-first splash

--- machine-precheck/sw.js
+++ machine-precheck/sw.js
@@ -1,5 +1,5 @@
-const CACHE='machine-precheck-prototype-v13';
-const SHELL=['./index.html','./app.css?v=13','./app.js?v=13','./manifest.webmanifest','./v8-sprite-60f.webp?v=13'];
+const CACHE='machine-precheck-prototype-v14';
+const SHELL=['./index.html','./app.css?v=14','./app.js?v=14','./manifest.webmanifest','./v8-sprite-60f.webp?v=14'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).catch(()=>{}));});
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',event=>{if(event.request.mode==='navigate'){event.respondWith(fetch(event.request).catch(()=>caches.match('./index.html')));return;}event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request)));});
