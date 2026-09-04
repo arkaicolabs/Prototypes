a22c6136ca0fe06b27b491c2b4b8fc1d213a72a0
utm-3426 <founder@arkaicolabs.com>
2026-09-04 17:39:00 +0100

    Rotate prototype cache for splash v8

--- machine-precheck/sw.js
+++ machine-precheck/sw.js
@@ -1,11 +1,5 @@
-const CACHE='machine-precheck-prototype-v7';
-const SHELL=['./index.html','./app.css?v=7','./app.js?v=7','./manifest.webmanifest','./v8-sprite-60f.webp?v=7'];
+const CACHE='machine-precheck-prototype-v8';
+const SHELL=['./index.html','./app.css?v=8','./app.js?v=8','./manifest.webmanifest','./v8-sprite-60f.webp?v=8'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).catch(()=>{}));});
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
-self.addEventListener('fetch',event=>{
-  if(event.request.mode==='navigate'){
-    event.respondWith(fetch(event.request).catch(()=>caches.match('./index.html')));
-    return;
-  }
-  event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request)));
-});
+self.addEventListener('fetch',event=>{if(event.request.mode==='navigate'){event.respondWith(fetch(event.request).catch(()=>caches.match('./index.html')));return;}event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request)));});
