87eccf2d38d15c6ca3da0eec5e176aad58113fbb
utm-3426 <founder@arkaicolabs.com>
2026-09-04 17:31:47 +0100

    Rotate prototype cache to v5

--- machine-precheck/sw.js
+++ machine-precheck/sw.js
@@ -1,5 +1,5 @@
-const CACHE='machine-precheck-prototype-v3';
-const SHELL=['./index.html','./app.css?v=3','./app.js?v=3','./manifest.webmanifest','./v8-sprite-60f.webp?v=3'];
+const CACHE='machine-precheck-prototype-v5';
+const SHELL=['./index.html','./app.css?v=5','./app.js?v=5','./manifest.webmanifest','./v8-sprite-60f.webp?v=5'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).catch(()=>{}));});
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',event=>{
