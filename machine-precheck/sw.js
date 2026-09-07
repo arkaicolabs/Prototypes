c1d439c1ebd07d83dbd3efe7571dd885053a39f1
utm-3426 <founder@arkaicolabs.com>
2026-09-07 11:25:52 +0100

    Refresh offline shell cache for minimal login

--- machine-precheck/sw.js
+++ machine-precheck/sw.js
@@ -1,4 +1,4 @@
-const CACHE='machine-precheck-prototype-v16';
+const CACHE='machine-precheck-prototype-v17';
 const SHELL=['./index.html','./app.css?v=16','./app.js?v=16','./manifest.webmanifest','./v8-sprite-60f.webp?v=16'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).catch(()=>{}));});
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
